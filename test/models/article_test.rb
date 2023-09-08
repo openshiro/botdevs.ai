b32d11dadd5d04fc06af313821ec8012181803d8
Duncan Miller <dmiller@heatspring.com>
2023-09-08 16:18:53 -0700

    adding tests for article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -5,9 +5,25 @@ class ArticleTest < ActiveSupport::TestCase
     @article = articles(:one)
   end
 
-  test "#draft? responds correctly " do
+  test "#draft? responds correctly based on published_at" do
+    refute @article.draft?
+
     @article.published_at = nil
 
     assert @article.draft?
+  end
+
+  test "#published? is true if published in past" do
+    @article.published_at = Date.yesterday
+
+    assert @article.published?
+    refute @article.scheduled?
+  end
+
+  test "#scheduled? is true if published in future" do
+    @article.published_at = Date.tomorrow
+
+    assert @article.scheduled?
+    refute @article.published?
   end
 end
