a391408169e697c44578b39a083c514e28a05a09
Duncan Miller <dmiller@heatspring.com>
2023-09-27 13:43:49 -0700

    add static author to articles

---
+++ db/seeds/articles.rb
@@ -0,0 +1,10 @@
+title = "A published article"
+attributes = {
+  title:,
+  published_at: Time.now,
+  content: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum. Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum. Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.\n\n Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum."
+}
+
+Article.find_or_create_by!(title:) do |article|
+  article.assign_attributes(attributes)
+end
