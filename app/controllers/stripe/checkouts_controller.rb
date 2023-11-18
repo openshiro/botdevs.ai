8b32254a54624ddb52f6b17c1b9f4440babdcf13
Duncan Miller <dmiller@heatspring.com>
2023-11-18 14:26:32 -0800

    Revert "disable require signed hiring agreement to checkout"
    This reverts commit 35cc30b73f28d65087e02137b69ad3e9340f0c12.

--- app/controllers/stripe/checkouts_controller.rb
+++ app/controllers/stripe/checkouts_controller.rb
@@ -1,7 +1,7 @@
 module Stripe
   class CheckoutsController < ApplicationController
     before_action :authenticate_user!
-    # before_action :require_signed_hiring_agreement!
+    before_action :require_signed_hiring_agreement!
 
     def create
       redirect_to BusinessSubscriptionCheckout.new(
@@ -13,10 +13,10 @@ module Stripe
 
     private
 
-    # def require_signed_hiring_agreement!
-    #   if current_user.needs_to_sign_hiring_agreement?
-    #     redirect_to new_hiring_agreement_signature_path, notice: I18n.t("errors.hiring_agreements.payment")
-    #   end
-    # end
+    def require_signed_hiring_agreement!
+      if current_user.needs_to_sign_hiring_agreement?
+        redirect_to new_hiring_agreement_signature_path, notice: I18n.t("errors.hiring_agreements.payment")
+      end
+    end
   end
 end
--- test/integration/hiring_agreements/signatures_test.rb
+++ test/integration/hiring_agreements/signatures_test.rb
@@ -23,22 +23,22 @@ class HiringAgreements::SignaturesTest < ActionDispatch::IntegrationTest
     assert_redirected_to hiring_agreement_terms_path
   end
 
-  # test "requires the agreement to be signed" do
-  #   sign_in users(:empty)
+  test "requires the agreement to be signed" do
+    sign_in users(:empty)
 
-  #   assert_no_changes "HiringAgreements::Signature.count" do
-  #     post hiring_agreement_signature_path, params: signature_params(signed: false)
-  #   end
-  # end
+    assert_no_changes "HiringAgreements::Signature.count" do
+      post hiring_agreement_signature_path, params: signature_params(signed: false)
+    end
+  end
 
-  # test "redirects to the stored location" do
-  #   sign_in users(:empty)
-  #   post stripe_checkout_path(plan: :full_time)
+  test "redirects to the stored location" do
+    sign_in users(:empty)
+    post stripe_checkout_path(plan: :full_time)
 
-  #   post hiring_agreement_signature_path, params: signature_params(signed: true)
+    post hiring_agreement_signature_path, params: signature_params(signed: true)
 
-  #   assert_redirected_to pricing_path
-  # end
+    assert_redirected_to pricing_path
+  end
 
   def signature_params(signed: true)
     {
