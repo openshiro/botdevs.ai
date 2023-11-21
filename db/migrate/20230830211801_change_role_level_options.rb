4794693ff77ae2ef4f39b2fc97e3ad366de1fc5a
Duncan Miller <dmiller@heatspring.com>
2023-11-20 16:38:39 -0800

    fixing find replace in migrations

--- db/migrate/20230830211801_change_role_level_options.rb
+++ db/migrate/20230830211801_change_role_level_options.rb
@@ -1,6 +1,6 @@
 class ChangeRoleLevelOptions < ActiveRecord::Migration[7.0]
   def change
-    rename_column :role_levels, :junior, :junior
+    rename_column :role_levels, :junior, :entry_level
     rename_column :role_levels, :mid, :apprentice
     rename_column :role_levels, :senior, :journeyman
     rename_column :role_levels, :principal, :master
