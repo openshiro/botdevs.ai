551c16d83ab6e99c13280acec90f3d369315973a
Duncan Miller <dmiller@heatspring.com>
2023-12-04 11:42:48 -0800

    updating test

--- test/jobs/markdown_file_job_test.rb
+++ test/jobs/markdown_file_job_test.rb
@@ -1,31 +1,32 @@
 # test/jobs/markdown_file_job_test.rb
-require 'test_helper'
+require "test_helper"
 
 class MarkdownFileJobTest < ActiveSupport::TestCase
   def setup
-    WebMock.disable_net_connect!(allow: ['raw.githubusercontent.com'])
-    VCR.use_cassette('markdown_file_job') do
+    WebMock.disable_net_connect!(allow: ["raw.githubusercontent.com"])
+    VCR.use_cassette("markdown_file_job") do
       @job = MarkdownFileJob.new
       @article = Article.learning_resources
     end
   end
 
-  test 'job updates article' do
+  test "job updates article" do
     assert_changes -> { [@article.title, @article.content.body.to_s] } do
       @job.perform
       @article.reload
     end
   end
 
-  test 'job updates article and removes the first H2 tag' do
-    @job.perform
-    @article.reload
+  test "job updates article and removes the first H2 tag" do
+    assert_changes -> { [@article.title, @article.content.body.to_s] } do
+      @job.perform
+      @article.reload
+    end
 
-    saved_html_content = @article.content
-    assert_not @article.content.body.to_s.include?('<h2>')
+    assert_not @article.content.body.to_s.include?("<h2>")
   end
 
-  test 'job does not update article if no changes' do
+  test "job does not update article if no changes" do
     @job.perform
     @article.reload
     initial_title = @article.title
