cac2a598a6ebc1d7e7d42af5fc2c48bc1e2b9910
Duncan Miller <dmiller@heatspring.com>
2023-12-04 00:06:34 -0800

    move redirect friendly id to application controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   def show
     @article = Article.friendly.find(params[:id])
-
-    if request.path != article_path(@article)
-      redirect_to article_path(@article)
-      return
-    end
-
+    redirect_resource_if_not_latest_friendly_id(@article)
     redirect_to root_path unless @article.published?
   end
 end
