a19c1971a477f29dc1d590fd522e0ff55f88f5fd
Duncan Miller <dmiller@heatspring.com>
2023-12-03 23:59:08 -0800

    update routes for learn title and redirect old article slugs to new slug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.friendly.find(params[:id])
 
+    if request.path != article_path(@article)
+      redirect_to article_path(@article)
+      return
+    end
+
     redirect_to root_path unless @article.published?
   end
 end
