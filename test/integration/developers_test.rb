6c77a8589dbc25a6cc1fc17fea517bb58fd806de
Duncan Miller <dmiller@heatspring.com>
2023-12-05 12:01:55 -0800

    adding debugging statements to flaky test

--- test/integration/developers_test.rb
+++ test/integration/developers_test.rb
@@ -39,10 +39,12 @@ class DevelopersTest < ActionDispatch::IntegrationTest
     dev2 = create_developer(hero: "Newest")
     dev1.update(created_at: 7.days.ago)
     dev2.update(created_at: Time.zone.now)
-    dev1.reload
-    dev2.reload
 
     get developers_path(sort: :newest)
+
+    puts "Response Body: #{response.body}"
+    puts "Index of Newest: #{response.body.index("Newest")}"
+    puts "Index of Oldest: #{response.body.index("Oldest")}"
 
     assert_select "button.font-medium[value=newest]"
     assert response.body.index("Newest") < response.body.index("Oldest")
