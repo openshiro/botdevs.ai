9f018e67f2cbae21aa1efc61d323453012d980e7
Duncan Miller <dmiller@heatspring.com>
2023-12-05 11:20:00 -0800

    add updated at to articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def show_updated?(article)
+    article.updated_at > article.created_at + 3.days
+  end
 end
---
+++ test/helpers/articles_helper_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+require "articles_helper"
+
+class ArticlesHelperTest < ActionView::TestCase
+  include ArticlesHelper
+
+  def setup
+    @article = Article.new
+  end
+
+  test "show_updated? returns true if updated more than 3 days after created" do
+    @article.created_at = Time.now - 5.days
+    @article.updated_at = Time.now
+
+    assert_equal true, show_updated?(@article)
+  end
+
+  test "show_updated? returns false if updated within 3 days after created" do
+    @article.created_at = Time.now
+    @article.updated_at = Time.now + 2.days
+
+    assert_equal false, show_updated?(@article)
+  end
+end
