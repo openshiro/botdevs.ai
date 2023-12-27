7cd5c6be2e940b674bb061897f12232e257e3297
Duncan Miller <dmiller@heatspring.com>
2023-12-26 21:12:16 -0800

    add bash as langague for highlight

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -4,6 +4,7 @@ import javascript from 'highlight.js/lib/languages/javascript';
 import erb from 'highlight.js/lib/languages/erb';
 import ruby from 'highlight.js/lib/languages/ruby';
 import python from 'highlight.js/lib/languages/python';
+import bash from 'highlight.js/lib/languages/bash';
 
 // Connects to data-controller="highlight"
 export default class extends Controller {
@@ -12,6 +13,7 @@ export default class extends Controller {
     hljs.registerLanguage('erb', erb);
     hljs.registerLanguage('ruby', ruby);
     hljs.registerLanguage('python', python);
+    hljs.registerLanguage('bash', bash);
 
     this.element.querySelectorAll('pre').forEach((element) => {
       hljs.highlightElement(element);
