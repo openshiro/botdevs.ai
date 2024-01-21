f306364a61cc9db53b017a8578443bf597e1e987
Duncan Miller <dmiller@heatspring.com>
2024-01-21 14:18:20 -0800

    adding redirect for the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,11 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   def show
     @article = Article.friendly.find(params[:id])
-    redirect_resource_if_not_latest_friendly_id(@article)
+    redirect_resource_if_not_latest_friendly_id(@article) and return
+    if @article.id == 6
+      redirect_to "https://openshiro.com/blog/prompt-engineering-testing-strategies-with-python", allow_other_host: true and return
+    end
+
     redirect_to root_path unless @article.published?
   end
 end
