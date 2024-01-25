ae3430cdeba675f2a67904c91fa1a989c128b709
Duncan Miller <dmiller@heatspring.com>
2024-01-24 23:54:00 -0800

    updating redirect url

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
     @article = Article.friendly.find(params[:id])
     redirect_resource_if_not_latest_friendly_id(@article) and return
     if @article.id == 6
-      redirect_to "https://openshiro.com/blog/prompt-engineering-testing-strategies-with-python", allow_other_host: true and return
+      redirect_to "https://openshiro.com/articles/prompt-engineering-testing-strategies-with-python", allow_other_host: true and return
     end
 
     redirect_to root_path unless @article.published?
